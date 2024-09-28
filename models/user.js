cff5854704a2684be2a63d6a88c12fd6a013f08a
wonvincal <calvin.kl.wong@gmail.com>
2024-09-29 00:18:48 +0800

    first commit

---
+++ models/user.js
@@ -0,0 +1,25 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists!'],
+        required: [true, 'Email is required!'],
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        $match: [
+            /^[A-Za-z][A-Za-z0-9_]{4,29}$/,
+            "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+          ]
+        // match: [/^(?=.{3,40}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    image: {
+        type: String,
+    }
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
