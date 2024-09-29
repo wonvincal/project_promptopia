96585ae901c593915399db896ae69f14d967cec1
wonvincal <calvin.kl.wong@gmail.com>
2024-09-29 15:46:23 +0800

    Revalidate path that fetches prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,17 @@
 import { connectToDB } from "@utils/database"
+import { NextRequest } from "next/server";
+import { revalidatePath } from "next/cache";
 import Prompt from "@models/prompt";
 
-export const GET = async (req) => {
+export const GET = async (request) => {
     try {
         await connectToDB();
 
         const prompts = await Prompt.find({}).populate('creator');
+
+        const path = request.nextUrl.searchParams.get("path") || "/";
+
+        revalidatePath(path);
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
